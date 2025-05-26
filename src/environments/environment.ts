b0dc45097cbb2baa0d50262d3b0b640476b775b0
Max Poblete <max.poblete2905@gmail.com>
2025-05-25 22:53:20 -0400

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-const PATH = 'https://52.207.110.85:8080'
+const PATH = 'http://3.89.125.201/:8080'
 export const environment = {
   production: false,
   apiSecret: 'a3f8d7e0b2c94f1e6d5a8c2b7e3f9d01a4c6b8d2e5f7a3c9d1e0b4f6a8c2d7',
