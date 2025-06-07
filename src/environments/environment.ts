97974b83303ed25764fed72a7dfa8e42fa0afb5b
Max Poblete <max.poblete2905@gmail.com>
2025-06-06 20:45:17 -0400

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,32 @@
-const PATH = 'https://3.89.125.201:8080'
+const PATH = 'http://3.89.125.201:8080'
 export const environment = {
   production: false,
   apiSecret: 'a3f8d7e0b2c94f1e6d5a8c2b7e3f9d01a4c6b8d2e5f7a3c9d1e0b4f6a8c2d7',
   apiUrlProviders: [
-    { provide: 'PROJECTS_API_URL', useValue: `${PATH}/projects` },
-    { provide: 'SKILLS_API_URL', useValue: `${PATH}/skills` },
-    { provide: 'PERSONAL_API_URL', useValue: `${PATH}/personal-information` },
-    { provide: 'TECHNOLOGY_API_URL', useValue: `${PATH}/technologies` },
-    { provide: 'ACADEMIC_API_URL', useValue: `${PATH}/academic` },
-    { provide: 'CERTIFICATION_API_URL', useValue: `${PATH}/certifications` },
-    { provide: 'CONTACT_API_URL', useValue: `${PATH}/contacts` }
-  ]
-};
+    {
+      provide: 'PROJECTS_API_URL',
+      useValue: `https://3.89.125.201:8080/projects`,
+    },
+    { provide: 'SKILLS_API_URL', useValue: `https://3.89.125.201:8080/skills` },
+    {
+      provide: 'PERSONAL_API_URL',
+      useValue: `https://3.89.125.201:8080/personal-information`,
+    },
+    {
+      provide: 'TECHNOLOGY_API_URL',
+      useValue: `https://3.89.125.201:8080/technologies`,
+    },
+    {
+      provide: 'ACADEMIC_API_URL',
+      useValue: `https://3.89.125.201:8080/academic`,
+    },
+    {
+      provide: 'CERTIFICATION_API_URL',
+      useValue: `https://3.89.125.201:8080/certifications`,
+    },
+    {
+      provide: 'CONTACT_API_URL',
+      useValue: `https://3.89.125.201:8080/contacts`,
+    },
+  ],
+}
